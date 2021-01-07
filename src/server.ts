5702054aad498b4b932c2e492393b40d85d9ec9d
edstngdv <eddingston.dev@gmail.com>
2021-01-07 00:13:31 -0800

    init

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+export default function startServer(): void {
+    const port = 3000
+
+    const app = express()
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+
+    app.get("/", (req, res) => {
+        res.send("200: OK");
+    });
+
+    app.listen(port, () => {
+        console.log(`Example app listening at http://localhost:${port}`)
+    })
+}
