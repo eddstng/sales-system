2b5fb476f4e1dc765a064b933030b90a05624e02
eddstng <eddstng@gmail.com>
2021-03-16 22:23:05 -0700

    added getAllOrderItem, getOneOrderItem, createOrderItem, deleteOneOrderItem, and updateOrderItem

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import { logger } from '../src/logging/logger';
 import { Prisma } from '@prisma/client';
 import { getAllCustomers, getOneCustomer, createCustomer, updateCustomer, deleteOneCustomer } from './handlers/customers/customers';
 import { createOrder, deleteOneOrder, getAllOrders, getOneOrder, updateOrder } from './handlers/orders/orders';
+import { getAllOrderItem, createOrderItem, updateOrderItem, deleteOneOrderItem, getOneOrderItem } from './handlers/order_items/orderItems';
 
 export default function startServer(): void {
     const port = 3000
@@ -120,6 +121,43 @@ export default function startServer(): void {
     app.delete('/delete/orders/delete/id/:id', async (req, res) => {
         try {
             res.status(200).json(await deleteOneOrder(parseInt(req.params.id)))
+        } catch (err) {
+            res.status(500).send({ error: err.toString() });
+        }
+    })
+
+    // OrdersItem
+    app.get('/get/orderitems/all', async (_req, res) => {
+        try {
+            res.status(200).json(await getAllOrderItem())
+        } catch (err) {
+            res.status(500).send({ error: err.toString() });
+        }
+    })
+    app.get('/get/orderitems/id/:id', async (req, res) => {
+        try {
+            res.status(200).json(await getOneOrderItem(parseInt(req.params.id)))
+        } catch (err) {
+            res.status(500).send({ error: err.toString() });
+        }
+    })
+    app.post('/post/orderitems/create', async (req, res) => {
+        try {
+            res.status(200).json(await createOrderItem(req.body))
+        } catch (err) {
+            res.status(500).send({ error: err.toString() });
+        }
+    })
+    app.put('/put/orderitems/update/id/:id', async (req, res) => {
+        try {
+            res.status(200).json(await updateOrderItem(parseInt(req.params.id), <Prisma.orders_itemsUncheckedUpdateInput>req.body))
+        } catch (err) {
+            res.status(500).send({ error: err.toString() });
+        }
+    })
+    app.delete('/delete/orderitems/delete/id/:id', async (req, res) => {
+        try {
+            res.status(200).json(await deleteOneOrderItem(parseInt(req.params.id)))
         } catch (err) {
             res.status(500).send({ error: err.toString() });
         }
