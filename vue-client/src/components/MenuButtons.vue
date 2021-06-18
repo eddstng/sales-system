b5b3e13bc3aa59ca7686abf79bb24adbf2911aa8
eddstng <eddstng@gmail.com>
2021-06-17 22:59:22 -0700

    client: added order button size popup placeholder

--- vue-client/src/components/MenuButtons.vue
+++ vue-client/src/components/MenuButtons.vue
@@ -16,8 +16,11 @@
             v-on:click="onClickMenuButton(item)"
           >
             <div>
-               {{ item.id }}<br /> <br />
-              {{ item.name_eng }}<br />{{ item.name_chn }}<br /><br />{{ item.price }}
+              {{ item.id }}<br />
+              <br />
+              {{ item.name_eng }}<br />{{ item.name_chn }}<br /><br />{{
+                item.price
+              }}
             </div>
           </v-btn>
 
@@ -30,34 +33,61 @@
                     x-large
                     height="180px"
                     width="50%"
-                    v-on:click="onClickMenuButton(item)"
+                    v-on:click="
+                      onClickMenuButton(item);
+                      dialog = true;
+                    "
                     v-bind="attrs"
                     v-on="on"
                   >
                     <div>
-                                     {{ item.id }}<br /> <br />
-                      {{ item.name_eng }}<br />{{ item.name_chn }}<br /><br />S: {{ item.sizes.small }} <br/> L: {{ item.sizes.large }} 
+                      {{ item.id }}<br />
+                      <br />
+                      {{ item.name_eng }}<br />{{ item.name_chn }}<br /><br />S:
+                      {{ item.sizes.small }} <br />
+                      L: {{ item.sizes.large }}
                     </div>
                   </v-btn>
                 </template>
+
                 <v-card>
                   <v-card-title class="headline grey lighten-2">
                     Privacy Policy
                   </v-card-title>
+
+                  <v-card-text>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                  </v-card-text>
+
+                  <v-divider></v-divider>
+
                   <v-card-actions>
                     <v-spacer></v-spacer>
+                    <v-btn color="primary" text @click="dialog = false">
+                      I accept
+                    </v-btn>
+
                     <v-btn
                       class="menu-button-text"
                       x-large
                       height="180px"
                       width="50%"
-                      v-on:click="onClickMenuButton(item)"
-                      v-bind="attrs"
-                      v-on="on"
-                      v-for="(size, index) of item.sizes"
-                      v-bind:key="index"
+                      v-on:click="
+                        onClickMenuButton(item);
+                        dialog = false;
+                      "
+                      v-for="(price, size) of item.sizes"
+                      v-bind:key="size"
+                      @click="dialog = false"
                     >
-                      <div>{{ index }}<br />{{ size }}<br /></div>
+                      <div>{{ size }}<br />{{ price }}<br /></div>
                     </v-btn>
                   </v-card-actions>
                 </v-card>
@@ -84,7 +114,9 @@ overflow: hidden;
 import { store } from "../store/store";
 export default {
   data() {
-    return {};
+    return {
+      dialog: false,
+    };
   },
   mounted() {
     this.scrollToElement();
