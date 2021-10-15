1da253383c244a5b6f88640f566dd5419f20f8cd
eddstng <eddstng@gmail.com>
2021-10-14 22:56:30 -0700

    client: updated menu buttons font

--- vue-client/src/components/MenuButtons.vue
+++ vue-client/src/components/MenuButtons.vue
@@ -2,7 +2,6 @@
   <v-card outlined tile class="overflow-y-auto" height="92.4vh">
     <div class="p-0" max-height="400">
       <v-btn
-        class="menu-button-text"
         v-for="item in $store.state.items"
         v-bind:key="item.name_eng"
         x-large
@@ -10,18 +9,25 @@
         height="180px"
         width="50%"
         v-on:click="onClickMenuButton(item)"
-        >{{ item.name_eng }}<br />{{ item.name_chn }}<br />{{
-          item.price
-        }}</v-btn
-      >
+        ><div>
+          <p class="menu-button-text-eng">{{ item.name_eng }}</p>
+          <p class="menu-button-text-chn">{{ item.name_chn }}</p>
+          <p class="menu-button-text-price">{{ item.price }}</p>
+        </div>
+      </v-btn>
     </div>
   </v-card>
 </template>
 
 <style>
-.menu-button-text {
+.menu-button-text-eng {
   font-size: 1.2em;
-  overflow: hidden;
+}
+.menu-button-text-chn {
+  font-size: 1.7em;
+}
+.menu-button-text-price {
+  font-size: 1.7em;
 }
 </style>
 
