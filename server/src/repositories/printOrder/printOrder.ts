32d4835fee32bd84fedfda3d3aebc986dbe2571a
eddstng <eddstng@gmail.com>
2022-08-20 14:55:43 -0700

    update logging

--- server/src/repositories/printOrder/printOrder.ts
+++ server/src/repositories/printOrder/printOrder.ts
@@ -1,8 +1,9 @@
 import { prisma } from '../../app'
-import { logInfo } from '../../logging/utils'
+import { logError, logInfo, logWarn } from '../../logging/utils'
 import { thermalPrinterInterface } from '../../app';
 import * as dotenv from 'dotenv';
 import { orders_items_detail } from '@prisma/client';
+import { logger } from '../../logging/logger';
 
 dotenv.config();
 const ThermalPrinter = require('node-thermal-printer').printer
@@ -11,6 +12,10 @@ const english = /^[A-Za-z\d_-]+$/;
 let printer: any;
 
 function thermalPrinterSetup(): any {
+    if (process.env.PRINTING !== 'true') {
+        logInfo(thermalPrinterSetup.name, 'Printing is set to false in env. Printer will not be initialized.')
+        return
+    }
     if (printer === undefined) {
         console.log('PRINTER INITIALIZED')
         printer = new ThermalPrinter({
@@ -47,16 +52,20 @@ export async function createAndPrintOrderBill(printObj: { order_id: number, prin
     try {
         const kitchenAndClientBills = await createKitchenAndClientBill(printObj.order_id)
         thermalPrinterSetup()
-        printer.clear()
+        if (printer !== undefined) {
+            printer.clear()
+        }
         // The delay is required to give time for the image to save. 
         const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
         await delay(10)
-        await billSetup(printer, printObj, kitchenAndClientBills)
-        printer.execute();
-        logInfo(createAndPrintOrderBill.name, `[✓]`)
+        if (process.env.PRINTING !== 'false') {
+            await billSetup(printer, printObj, kitchenAndClientBills)
+            printer.execute();
+        }
+        logInfo(createAndPrintOrderBill.name, `Success`)
         return;
     } catch (err) {
-        logInfo(createAndPrintOrderBill.name, `[✗] ${err}`)
+        logError(createAndPrintOrderBill.name, `${err}`)
         throw err;
     }
 
@@ -67,7 +76,15 @@ async function getOrderToPrint(order_id: number, retries: number, err?: unknown 
     if (retries > 3 && err) {
         throw new Error(err as string)
     }
+    if (retries > 0) {
+        logWarn(getOrderToPrint.name, `Unable to print order due to error. Retry: ${retries}/3`)
+    }
     try {
+        // if (retries <= 3) {
+        //     const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+        //     await delay(1000)
+        //     throw new Error('fake error')
+        // }
         const res = await prisma.orders_items_detail.findMany(
             {
                 where: {
@@ -77,17 +94,20 @@ async function getOrderToPrint(order_id: number, retries: number, err?: unknown 
         )
         return res
     } catch (err) {
-        console.log('=============================')
-        console.log(`ALERT: prisma.orders_items_detail.findMany has failed during createAndPrintOrderBill retrying. ${retries++}/3`)
-        console.log('=============================')
-        return getOrderToPrint(order_id, retries++, err)
+        if (retries === 0) {
+            logWarn(getOrderToPrint.name, 'Unable to print order due to error. Attempting to retry...')
+            logError(getOrderToPrint.name, `${err}`)
+        } else {
+            logError(getOrderToPrint.name, `${err}`)
+
+        }
+        return getOrderToPrint(order_id, retries + 1, err)
     }
 }
 
 export async function createKitchenAndClientBill(order_id: number): Promise<{ clientBillPath: string, kitchenBillPath: string, isDelivery: boolean }> {
     try {
         const res = await getOrderToPrint(order_id, 0)
-
         let orderTypeString = 'PICK UP'
         if (res[0].order_type === 0) {
             orderTypeString = 'DINE IN'
@@ -172,7 +192,7 @@ export async function createKitchenAndClientBill(order_id: number): Promise<{ cl
         kitchenBillString += `
 
         `
-        
+
         clientBillString += `
         -----------------------
         Number of Items: ${res[0].items_quantity_total}
@@ -200,10 +220,15 @@ export async function createKitchenAndClientBill(order_id: number): Promise<{ cl
         imageDataURI.outputFile(kitchenBillImageURI, kitchenBillPath)
         imageDataURI.outputFile(clientBillImageURI, clientBillPath)
 
-        logInfo(createKitchenAndClientBill.name, `[✓]`)
+        logInfo(createKitchenAndClientBill.name, `Success`)
         return { clientBillPath, kitchenBillPath, isDelivery: res[0].order_type === 2 };
     } catch (err) {
-        logInfo(createKitchenAndClientBill.name, `[✗] ${err}`)
+
+
+        // How far along in the process did we get to before failure?
+        // does it matter or should we just delete all from this order and try again?
+        // if an error happens we want to make sure that we have all the data required to go back to reordering this order.
+        logError(createKitchenAndClientBill.name, `${err}`)
         throw err;
     }
 }
