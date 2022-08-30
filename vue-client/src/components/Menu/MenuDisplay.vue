e2990ae8d67c8abedad491776eccb2f3f35e7799
eddstng <eddstng@gmail.com>
2022-08-30 00:39:06 -0700

    update MenuDisplay to display custom name if available instead of name_eng

--- vue-client/src/components/Menu/MenuDisplay.vue
+++ vue-client/src/components/Menu/MenuDisplay.vue
@@ -21,7 +21,7 @@
         >
           <v-list-item three-line v-if="item.node !== undefined">
             <v-list-item-content>
-              <div class="menu-display-item-text">{{ item.node.name_eng }}</div>
+              <div class="menu-display-item-text">{{ item.node.custom_name || item.node.name_eng }}</div>
               <div class="menu-display-item-text">{{ item.node.name_chn }}</div>
             </v-list-item-content>
             <v-list-item-content>
