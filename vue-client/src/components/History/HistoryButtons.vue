05a92ff4cac0a9c922a283925a992eb38721155f
eddstng <eddstng@gmail.com>
2022-10-17 23:27:38 -0700

    fix issue with seperating item id for items with customizations

--- vue-client/src/components/History/HistoryButtons.vue
+++ vue-client/src/components/History/HistoryButtons.vue
@@ -1,27 +1,43 @@
 <template>
   <v-card outlined tile class="overflow-y-auto" height="90.4vh" v-chat-scroll>
-    <div v-if="Object.keys($store.state.orderHistory).length === 0" class="p-0" max-height="400">
-      <p class="text-subtitle-2 text-center pt-35p">
-        NO ORDERS FOUND
-      </p>
+    <div
+      v-if="Object.keys($store.state.orderHistory).length === 0"
+      class="p-0"
+      max-height="400"
+    >
+      <p class="text-subtitle-2 text-center pt-35p">NO ORDERS FOUND</p>
     </div>
     <div v-else class="p-0" max-height="400">
-      <v-btn class="history-button-text" v-for="order in $store.state.orderHistory" v-bind:key="order.order_id" x-large
-        dark height="100px" width="100%" v-on:click="onClickHistoryButton(order.order_id)">
+      <v-btn
+        class="history-button-text"
+        v-for="order in $store.state.orderHistory"
+        v-bind:key="order.order_id"
+        x-large
+        dark
+        height="100px"
+        width="100%"
+        v-on:click="onClickHistoryButton(order.order_id)"
+      >
         <v-row>
           <v-col>
             {{ new Date(order.order_timestamp).toLocaleDateString() }}
             {{ getFormattedTimeStamp(new Date(order.order_timestamp)) }}
           </v-col>
           <!-- <v-col> {{ order.order_id }} </v-col> -->
-          <v-col> {{ order.order_number ? order.order_number : `I - ` + order.order_internal_number }}  </v-col>
+          <v-col>
+            {{
+              order.order_number
+                ? order.order_number
+                : `I - ` + order.order_internal_number
+            }}
+          </v-col>
           <v-col> {{ orderTypeString[order.order_type] }} </v-col>
           <v-col v-if="order.order_void === true"> VOID </v-col>
           <v-col v-else-if="order.order_paid === true"> PAID </v-col>
           <v-col v-else> </v-col>
           <v-col>
             {{
-                order.customer_phone.replace(/(\d{3})(\d{3})(\d{3})/, '$1-$2-$3')
+              order.customer_phone.replace(/(\d{3})(\d{3})(\d{3})/, "$1-$2-$3")
             }}
           </v-col>
           <v-col> {{ order.order_total.toFixed(2) }} </v-col>
@@ -43,13 +59,13 @@
 </style>
 
 <script>
-import storeMixin from '../../mixins/storeMixin';
-import axios from 'axios';
-import { store } from '../../store/store';
+import storeMixin from "../../mixins/storeMixin";
+import axios from "axios";
+import { store } from "../../store/store";
 export default {
   mixins: [storeMixin],
   data() {
-    return { orderTypeString: ['DINE IN', 'PICK UP', 'DELIVERY'] };
+    return { orderTypeString: ["DINE IN", "PICK UP", "DELIVERY"] };
   },
   created() {
     this.$root.$refs.MenuButtons = this;
@@ -58,20 +74,25 @@ export default {
     getFormattedTimeStamp: function (orderTimestamp) {
       var hours = orderTimestamp.getHours();
       var minutes = orderTimestamp.getMinutes();
-      var ampm = hours >= 12 ? 'pm' : 'am';
+      var ampm = hours >= 12 ? "pm" : "am";
       hours = hours % 12;
       hours = hours ? hours : 12; // the hour '0' should be '12'
-      minutes = minutes < 10 ? '0' + minutes : minutes;
-      var strTime = hours + ':' + minutes + ' ' + ampm;
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      var strTime = hours + ":" + minutes + " " + ampm;
       return strTime;
     },
     // The parameter ordersItemsDetailWithOrderId is an object.
     addHistoryItemToSelectedItems(ordersItemsDetailWithOrderId) {
-      const displayId = ordersItemsDetailWithOrderId.item_id;
+      const displayId =
+        ordersItemsDetailWithOrderId.orders_items_customizations !== null
+          ? `${ordersItemsDetailWithOrderId.item_id}${JSON.stringify(
+              ordersItemsDetailWithOrderId.orders_items_customizations
+            )}`
+          : ordersItemsDetailWithOrderId.item_id;
       let selectedItems = store.state.selectedItems;
 
-      // Create a key value pair for the custom item. 
-      // The key will be used to distinguish between different items on the display. Custom items and items with customizations will have a customized unique key. Otherwise, the key will be the id of the item. This is also necessary for quantity updates. 
+      // Create a key value pair for the custom item.
+      // The key will be used to distinguish between different items on the display. Custom items and items with customizations will have a customized unique key. Otherwise, the key will be the id of the item. This is also necessary for quantity updates.
       selectedItems[displayId] = {};
       selectedItems[displayId].node = {
         id: ordersItemsDetailWithOrderId.item_id,
@@ -91,7 +112,7 @@ export default {
         ordersItemsDetailWithOrderId.orders_items_timestamp
       ).getTime(); // TO DO: Do we need this? I don't think so.
       // Update the $store.state.selectedItems.
-      store.commit('setSelectedItems', selectedItems);
+      store.commit("setSelectedItems", selectedItems);
     },
     async onClickHistoryButton(order_id) {
       // Clear $store.state.selectedCustomer, $store.state.selectedItems, and $store.state.currentOrder.
@@ -102,12 +123,12 @@ export default {
           `http://localhost:3000/get/ordersitemsdetail/id/${order_id}`
         )
       ).data;
-      // Add each item to our $store.state.selectedItems. 
+      // Add each item to our $store.state.selectedItems.
       ordersItemsDetailWithOrderIdArray.forEach((v) => {
         this.addHistoryItemToSelectedItems(v);
       });
-      // Update the $store.state.currentOrder. 
-      store.commit('setCurrentOrder', {
+      // Update the $store.state.currentOrder.
+      store.commit("setCurrentOrder", {
         id: ordersItemsDetailWithOrderIdArray[0].order_id,
         type: ordersItemsDetailWithOrderIdArray[0].order_type,
         total: ordersItemsDetailWithOrderIdArray[0].order_total,
@@ -117,10 +138,11 @@ export default {
         timestamp: ordersItemsDetailWithOrderIdArray[0].order_timestamp,
         internal: ordersItemsDetailWithOrderIdArray[0].order_internal,
         number: ordersItemsDetailWithOrderIdArray[0].order_number,
-        internal_number: ordersItemsDetailWithOrderIdArray[0].order_internal_number,
+        internal_number:
+          ordersItemsDetailWithOrderIdArray[0].order_internal_number,
       });
       // Update the $store.state.selectedCustomer.
-      store.commit('setSelectedCustomer', {
+      store.commit("setSelectedCustomer", {
         address: ordersItemsDetailWithOrderIdArray[0].customer_address,
         city: ordersItemsDetailWithOrderIdArray[0].customer_city,
         id: ordersItemsDetailWithOrderIdArray[0].customer_id,
@@ -131,9 +153,10 @@ export default {
         street_number:
           ordersItemsDetailWithOrderIdArray[0].customer_street_number,
         unit_number: ordersItemsDetailWithOrderIdArray[0].customer_unit_number,
-        buzzer_number: ordersItemsDetailWithOrderIdArray[0].customer_buzzer_number,
+        buzzer_number:
+          ordersItemsDetailWithOrderIdArray[0].customer_buzzer_number,
       });
-      // Provide a total item count to the $store.state.currentOrder details. 
+      // Provide a total item count to the $store.state.currentOrder details.
       this.storeMixinSumSelectedItemsQuantity();
       // Provide price details to $store.state.priceDetails.
       this.storeMixinUpdateStorePriceDetails();
